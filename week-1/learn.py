ea1d48e415b7dab42651385be7969f45dc182690
smaiiil <ismailelbelrhiti1@gmail.com>
2026-07-15 08:44:19 +0800

    add lines

--- week-1/learn.py
+++ week-1/learn.py
@@ -1,2 +1,25 @@
-x = 'Argentina won the world cup'
-print('won' in x)
+#x = 'Argentina won the world cup'
+#print('won' in x)
+
+#x = 'Messi '
+#y = 'lionel '
+#sentence = y + x
+#print(sentence)
+
+#txt = 'PSG won the champions league'
+#txt.split()
+#txt.replace('PSG', 'Real Madrid')
+ 
+#name = 'LAMINE' 
+#from unicodedata import name
+
+
+#from os import name
+
+
+#team = 'FCBARCELONA'
+#position = 'RW'
+#goals = 13 
+#assists = 10 
+#print(f"{name} is a {position} player playing in {team} scored {goals}G and has {assists}A") 
+
