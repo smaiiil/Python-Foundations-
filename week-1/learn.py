c7efff08a51b7c929b8701135e91c397615e3b61
smaiiil <ismailelbelrhiti1@gmail.com>
2026-07-15 09:05:13 +0800

    add lines

--- week-1/learn.py
+++ week-1/learn.py
@@ -23,3 +23,19 @@
 #assists = 10 
 #print(f"{name} is a {position} player playing in {team} scored {goals}G and has {assists}A") 
 
+#player_name = 'ISMAIL'
+#team = 'WAC'
+#goals = 20
+#xG = 2.4
+#is_captain = 'true' 
+#print(type(xG))
+
+#player_name = 'LEO '
+#goals = 12 
+#print(player_name + "has scored" + str(goals) + "goals")
+
+#total_goals = 120
+#seasons = 5
+#average = total_goals / seasons
+#print(average)
+
